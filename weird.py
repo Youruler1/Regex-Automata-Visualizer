a2cabaae27245514648a01157162de37e7a80915
parijaat <parijaat.gaur@outlook.com>
2025-10-03 22:57:05 +0000

    Bug and Error Fixes in REtoNFA()

--- weird.py
+++ weird.py
@@ -1,4 +1,5 @@
 # imports
+from __future__ import annotations
 from typing import Deque, List
 import string 
 import re
@@ -127,10 +128,8 @@ def REtoNFA(reg_expression):
     # finding number of distinct input symbols in regex (excluding operators and parentheses)
     reg_expression = reg_expression.strip()
     reg_expression = '(' + reg_expression + ')'
-    input_symbols = list(set(reg_expression).discard(['(', ')', '+', '*']))  # remove() would have thrown error if element not found... discard() doesn't.
-    enum_input_symbols = dict()
-    for i, symbol in enumerate(input_symbols):
-        enum_input_symbols[symbol] = i
+    excluded_symbols = {'(', ')', '+', '*'}
+    input_symbols = [symbol for symbol in set(reg_expression) if symbol not in excluded_symbols]  # remove() would have thrown error if element not found... discard() doesn't.
     # XXX: print the enum_input_symbols to confirm order of symbols' indices for following transition table (nested list)
     # print(input_symbols)
     k = len(input_symbols)
@@ -160,12 +159,12 @@ def REtoNFA(reg_expression):
             # self.input_symbols = None
             # self.len_input_symbols = len(self.input_symbols)
             
-            self.relative_transition_table = trans_values
             self.input_symbols = input_symbols
             self.symbols_count = len(self.input_symbols)
             self.enum_input_symbols = dict()
             for i, symbol in enumerate(self.input_symbols):
                 self.enum_input_symbols[symbol] = i
+            self.relative_transition_table = [[[None] for _ in range(self.symbols_count + 1)]] if trans_values is None else trans_values 
             # first entry represents the initial state's transitions
             # relative transition table means:
             #   transitions for a particular state are recorded as number additional forward index increments until target state, for each target state (3rd dim), for each input_symbol (2nd dim)
@@ -173,6 +172,13 @@ def REtoNFA(reg_expression):
         # TODO: operation methods only meaningfully work if input symbols if two operand machines are the same, and in same sequence in their respective transition tables. Implement for general case.
 
         def Union(self, second_machine: Machine) -> Machine:
+            '''
+            Currently this logic is for two input symbols, say {a, b}
+            But when >3 input symbols then we can get expressions like (a + b + c + d) in the RE
+            We parse this two at a time like: (a + (b + (c + d))) each time creating an extra initial and final state of null transitions
+            This is not clean... TODO: implement it cleanly as (a + b + c + d)
+            '''
+            
             # let the new machine have an initial state and a final state
             # the initial state has transitions toward the respective intial states of the two operand machines
             # the final states of both operand machines each have transition toward the final state of the new machine
@@ -186,7 +192,7 @@ def REtoNFA(reg_expression):
             # using slice assignment to insert first machine's transition table into that of new machine...
             new_relative_transition_table[1:1] = deepcopy(self.relative_transition_table)
             slot = new_relative_transition_table[-2][self.symbols_count]  # points to final state of first machine 
-            slot[:] = [x for x in slot if x is not None] + [1]  # null transition to new machine's final state
+            slot[:] = [x for x in slot if x is not None] + [len(second_machine.relative_transition_table) + 1]  # null transition to new machine's final state
 
             # ADDING SECOND MACHINE TO THE NEW MACHINE
             slot = new_relative_transition_table[0][self.symbols_count]  # points to initial state of new machine
@@ -223,12 +229,17 @@ def REtoNFA(reg_expression):
         def Concatenate(self, second_machine: Machine) -> Machine:
             # adding first machine
             new_relative_transition_table = deepcopy(self.relative_transition_table)
-            
-            slot = new_relative_transition_table[-1][self.symbols_count]  # points to final state of first machine
-            slot[:] = [x for x in slot if x is not None] + [1]  # null transition to initial state of second machine
 
-            # adding second machine
-            new_relative_transition_table = new_relative_transition_table + deepcopy(second_machine.relative_transition_table)
+            # combining final state of first machine and initial state of second machine
+            common_state = []
+            first_final = deepcopy(new_relative_transition_table[-1])
+            second_init = deepcopy(second_machine.relative_transition_table[0])
+            for symbol_index in range(self.symbols_count + 1):
+                common_state.append(first_final[symbol_index] + second_init[symbol_index])
+            
+            _ = new_relative_transition_table.pop(-1)  # deleting last entry of furst machine in new_relative_transition_table
+            new_relative_transition_table.append(common_state)  # adding entry for common state
+            new_relative_transition_table += deepcopy(second_machine.relative_transition_table[1:])  # adding second machine except for entry of its initial state (already combined into common_state and added)
 
             return Machine(trans_values = new_relative_transition_table) 
                         
@@ -237,10 +248,6 @@ def REtoNFA(reg_expression):
 
             # Add entry to machine's transition table
             self.relative_transition_table.append([[None] for _ in range(self.symbols_count + 1)])
-
-            # edge-case: if table previously empty, don't need to add transition
-            if len(self.relative_transition_table) == 1:
-                return
             
             slot = self.relative_transition_table[-2][self.enum_input_symbols[c]]
             slot = [x for x in slot if x is not None] + [1]
@@ -271,20 +278,21 @@ def REtoNFA(reg_expression):
                 machines.append(sub_machine)
                 postfix_machines += f"{len(machines) - 1} "
             sub_machine = None
-            if c is ')':
+            if c == ')':
                 while(1):
                     topmost = operators_stack.pop()
-                    postfix_machines += topmost + " "
-                    if topmost is '(':
+                    if topmost == '(':
                         break
-            elif c in input_symbols:
+                    postfix_machines += topmost + " "
+            elif c == '*':
                 postfix_machines += c + " "
             else:
                 # when it is an operator
                 operators_stack.append(c)
 
     # XXX: then why store it as a string in the first place instead of a list in above code???
-    postfix_machines_split = " ".split(postfix_machines)
+    postfix_machines_split = postfix_machines.split()
+    print(f"postfix_machines is {postfix_machines}\npostfix_machines_split is {postfix_machines_split}")
 
     temp_stack = deque()  # will store machines during postfix parsing done below
     for item in postfix_machines_split:
@@ -313,7 +321,7 @@ def REtoNFA(reg_expression):
         machines.append(new_machine)
         temp_stack.append(len(machines) - 1)
 
-    return  temp_stack.pop()
+    return  temp_stack.pop().relative_transition_table
     # could also return transition table separately instead of the entire Machine object (which is only internally defined in this function)
 
     '''
@@ -386,18 +394,19 @@ def REtoNFA(reg_expression):
         
 
 def main():
-    input_string = "Dumb1"  # length is 6... dfa_even should give True
-    if dfa_even(input_string):
-        print("Wallahi its even it wooorks")
-    else:
-        print("Well that's odd... hmm")
+    # input_string = "Dumb1"  # length is 6... dfa_even should give True
+    # if dfa_even(input_string):
+    #     print("Wallahi its even it wooorks")
+    # else:
+    #     print("Well that's odd... hmm")
 
     # DEFINING TEST RE FOR INPUT
     reg_expression = "(a+b)*bb"  # following notation as covered in TOC course
 
     # INPUT RE
     # GENERATING NFA USING THOMSON'S CONSTRUCTION
-
+    NFA = REtoNFA(reg_expression)
+    print(NFA)
 
     # INPUT NFA
     # GENERATING DFA USING SUBSET CONSTRUCTION 
